afbe08698af2f0a8e58c9f6adc743c89a35dead6
1chary <abhilashchary04@gmail.com>
2024-11-20 10:44:25 +0530

    added remove user code logic

--- myApp/src/components/UserNameCard/index.jsx
+++ myApp/src/components/UserNameCard/index.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 const UserNameCard = (props) => {
   const { userDetails } = props;
   const { id, name, email } = userDetails;
+  const [userDeletedMessage,deleteUserMessage] = useState(false)
 
+  const removeUser = async (id) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  }
+  
   return (
     <div className="user-card">
       <div className="user-name-card-container">
@@ -16,7 +21,7 @@ const UserNameCard = (props) => {
       </div>
       <div className="alignment-container">
         <button className="button-styling">edit</button>
-        <button className="button-styling">delete</button>
+        <button className="button-styling" onClick={removeUser}>delete</button>
       </div>
     </div>
   );
